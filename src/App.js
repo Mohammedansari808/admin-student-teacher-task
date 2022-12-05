18d65178d02baa04ee257dd29e206e6693d4b7ef
Mohammedansari808 <mohammedansari808@gmail.com>
2022-12-05 21:41:02 +0530

    task uploaded

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import Student from './Student';
+import { Routes, Route, Link } from "react-router-dom";
+import Teachers from './Teachers';
+import NotFound from './NotFound';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Button from '@mui/material/Button';
 
 function App() {
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <Button color="inherit"><Link style={{ textDecoration: 'none', color: 'white' }} to="/">Home</Link></Button>
+
+            <Button color="inherit"><Link style={{ textDecoration: 'none', color: 'white' }} to="/student-login">Students Login</Link></Button>
+            <Button color="inherit"><Link style={{ textDecoration: 'none', color: 'white' }} to="/teacher-login">Teachers Login</Link></Button>
+
+          </Toolbar>
+        </AppBar>
+      </Box>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/student-login" element={<Student />} />
+        <Route path="/teacher-login" element={<Teachers />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
+
+  function Home() {
+    return (
+      <h1 >Welcome to the xyz college website</h1>
+
+    )
+  }
 }
 
 export default App;
